2557adea65f244291496051550f5cdcf35ab3756
NITRO\erry <errysyzwn@gmail.com>
2025-03-03 09:59:33 +0800

    update and insert privileges

--- employee.sql
+++ employee.sql
@@ -6,8 +6,8 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS employee_db;
 USE employee_db;
 
--- Table structure for table `employees`
-CREATE TABLE employees (
+-- Table structure for `employees`
+CREATE TABLE IF NOT EXISTS employees (
     `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(100) NOT NULL,
     `address` VARCHAR(255) NOT NULL,
@@ -16,14 +16,19 @@ CREATE TABLE employees (
 );
 
 -- Inserting data into `employees`
-INSERT INTO `employees` (`id`, `name`, `address`, `salary`) VALUES
-(1, 'Fatimah', 'Kuala Lumpur', 7000),
-(2, 'Azzahara', 'Selangor', 3500),
-(3, 'Mohd', 'Pahang', 9000),
-(4, 'Saiffuddin', 'Pahang', 5000);
+INSERT INTO employees (`name`, `address`, `salary`) VALUES
+('Fatimah', 'Kuala Lumpur', 7000),
+('Azzahara', 'Selangor', 3500),
+('Mohd', 'Pahang', 9000),
+('Saiffuddin', 'Pahang', 5000);
 
 -- Ensuring AUTO_INCREMENT continues correctly
-ALTER TABLE `employees`
-    MODIFY `id` INT NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE employees AUTO_INCREMENT = 5;
+
+-- --------------------------------------------
+ALTER USER 'root'@'localhost' IDENTIFIED BY '';
+FLUSH PRIVILEGES;
 
 COMMIT;
+
+EXIT;
