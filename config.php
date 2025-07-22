b9062c475a402d96982424a8887d3128850a56c7
NITRO\erry <errysyzwn@gmail.com>
2025-07-22 20:31:30 +0800

    Config the new DB

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
+define('DB_SERVER', 'employeedb.c61nvshwsdjh.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
 define('DB_PASSWORD', 'Demo.1029'); 
 define('DB_NAME', 'employee_db');
  
